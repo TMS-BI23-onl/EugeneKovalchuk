9c9fcf65432e1917066d3e77b58c3b2c94ed52a2
eugeenekovalchuk <eugenekovalchuk81@gmail.com>
2025-01-20 14:11:50 +0300

    ДЗ 2

---
+++ ZADANIE 1.sql
@@ -0,0 +1,15 @@
+SELECT *
+FROM Sales.SalesTerritory
+
+SELECT TerritoryID, Name 
+FROM Sales.SalesTerritory
+
+SELECT *
+FROM Person.Person
+WHERE LastName = 'Norman'
+
+SELECT *
+FROM Person.Person
+WHERE EmailPromotion < 2
+
+
---
+++ ZADANIE 4.sql
@@ -0,0 +1,41 @@
+SELECT COUNT(DISTINCT PersonType) AS UniquePersonTypes /*«¿ƒ¿Õ»≈ 1*/
+FROM Person.Person
+WHERE LastName LIKE 'M%' 
+   OR EmailPromotion <> 1;
+
+
+SELECT TOP 3 *   /* «¿ƒ¿Õ»≈ 2*/
+FROM Sales.SpecialOffer
+WHERE StartDate BETWEEN '2013-01-01' AND '2014-01-01'
+ORDER BY DiscountPct DESC
+
+
+SELECT MIN (Weight) AS MinWeight, /* «¿ƒ¿Õ»≈ 3*/
+	   MAX (Size) AS MaxSize
+FROM Production.Product
+
+
+SELECT MIN (Weight) AS MinWeight, /* «¿ƒ¿Õ»≈ 4*/
+	   MAX (Size) AS MaxSize, 
+	   ProductSubcategoryID
+FROM Production.Product 
+GROUP BY  ProductSubcategoryID
+
+
+SELECT MIN (Weight) AS MinWeight, /* «¿ƒ¿Õ»≈ 5*/
+	   MAX (Size) AS MaxSize, 
+	   ProductSubcategoryID,
+	   color
+FROM Production.Product
+WHERE Color IS NOT NULL
+GROUP BY  ProductSubcategoryID, Color
+
+
+
+
+
+
+
+
+
+
